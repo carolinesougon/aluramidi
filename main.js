ecf1475d4b97dfe98486bb3ffa2215ddc258083a
carolinesougon <114413234+carolinesougon@users.noreply.github.com>
2023-02-07 21:30:57 -0300

    aluramidi Aula 2

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-document.querySelector('.tecla_pom');
+// Criando a função:
+
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+// Chamando a função:
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+
+
